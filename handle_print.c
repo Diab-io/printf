3a31d5ac145063482ac0a139a5c355c493765400
Diab <odimayodavid7@gmail.com>
2022-07-12 14:44:12 +0100

    updated file

--- handle_print.c
+++ handle_print.c
@@ -7,8 +7,8 @@
  * @buffer: Buffer array to handle print.
  * @flags: Calculates active flags
  * @width: get width.
- * @precision: Precision specification
- * @size: Size specifier
+ * @precision: Precision specification.
+ * @size: Size specifier.
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
