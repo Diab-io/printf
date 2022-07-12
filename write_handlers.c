b76ba357f37e17aea96a5948028b054383be727d
Diab <odimayodavid7@gmail.com>
2022-07-12 14:47:11 +0100

    updated file

--- write_handlers.c
+++ write_handlers.c
@@ -8,7 +8,7 @@
  * @flags:  Calculates active flags.
  * @width: get width.
  * @precision: precision specifier
- * @size: Size specifier
+ * @size: Size specifier.
  *
  * Return: Number of chars printed.
  */
