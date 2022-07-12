fd11c33740c10e1ba554031d237044d683472710
Diab <odimayodavid7@gmail.com>
2022-07-12 14:42:13 +0100

    updated file

--- functions2.c
+++ functions2.c
@@ -7,8 +7,8 @@
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
+ * @precision: Precision specification.
+ * @size: Size specifier.
  * Return: Number of chars printed.
  */
 int print_pointer(va_list types, char buffer[],
